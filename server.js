8bce74c4e84d58cd8555f103ee0f94ee4b3b3210
JoaoMarceloRP <marcelorp1801@gmail.com>
2024-03-24 14:50:50 -0300

    Login system

--- server.js
+++ server.js
@@ -3,6 +3,23 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const path = require('path');
 
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'evepyeiadb'
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Erro ao conectar ao banco de dados:', err);
+        return;
+    }
+    console.log('Conexão com o banco de dados MySQL estabelecida');
+});
+
 const app = express();
 const port = 3000;
 
@@ -26,12 +43,47 @@ function requireAuth(req, res, next) {
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
 
-    if (username === 'user' && password === '1234') {
-        req.session.user = username;
-        res.status(200).json({ message: 'Login bem-sucedido' });
-    } else {
-        res.status(401).json({ message: 'Credenciais inválidas' });
-    }
+    connection.query('SELECT * FROM usuarios WHERE nome = ? AND senha = ?', [username, password], (error, results) => {
+        if (error) {
+            console.error('Erro ao verificar as credenciais:', error);
+            res.status(500).json({ message: 'Erro interno do servidor' });
+            return;
+        }
+
+        if (results.length === 1) {
+            req.session.user = username;
+            res.status(200).json({ message: 'Login bem-sucedido' });
+        } else {
+            res.status(401).json({ message: 'Credenciais inválidas' });
+        }
+    });
+});
+
+
+app.post('/signup', (req, res) => {
+    const { username, password, email, address, cpf } = req.body;
+
+    connection.query('SELECT * FROM usuarios WHERE nome = ?', [username], (error, results) => {
+        if (error) {
+            console.error('Erro ao verificar nome de usuário:', error);
+            res.status(500).json({ message: 'Erro interno do servidor' });
+            return;
+        }
+
+        if (results.length > 0) {
+            res.status(400).json({ message: 'Nome de usuário já em uso' });
+            return;
+        }
+
+        connection.query('INSERT INTO usuarios (nome, senha, email, endereco, cpf) VALUES (?, ?, ?, ?, ?)', [username, password, email, address, cpf], (error, results) => {
+            if (error) {
+                console.error('Erro ao criar conta:', error);
+                res.status(500).json({ message: 'Erro interno do servidor' });
+                return;
+            }
+            res.status(200).json({ message: 'Conta criada com sucesso' });
+        });
+    });
 });
 
 app.get('/home.html', requireAuth, (req, res) => {
